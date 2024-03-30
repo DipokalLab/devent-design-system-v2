ceaad1a1d6ee4491f03b3c6c6e13a0dee7d26924
Dipokal HHJ <hhj@devent.kr>
2024-03-30 21:47:20 +0900

    feat: add collapse component

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -2,10 +2,112 @@
 
 import React, { useEffect, useState } from "react";
 
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
+import { Box } from "./Box";
+
+const rotate180 = keyframes`
+0% {
+    transform: rotate(0deg);
+
+}
+  100% {
+    transform: rotate(180deg);
+  }
+`;
+
+const rotate0 = keyframes`
+0% {
+    transform: rotate(180deg);
+
+}
+  100% {
+    transform: rotate(0deg);
+  }
+`;
 
 function Collapse({ children }: { children?: any }) {
-  return <div></div>;
+  return (
+    <div
+      css={css({
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        width: "100%",
+      })}
+    >
+      {children}
+    </div>
+  );
 }
 
-export { Collapse };
+function CollapseItem({ children, title }: { children?: any; title?: any }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [rotateAnimation, setRotateAnimation] = useState(
+    `${rotate0} 0.3s forwards`
+  );
+  const handleClickTitle = () => {
+    if (isOpen) {
+      setRotateAnimation(`${rotate0} 0.3s forwards`);
+    } else {
+      setRotateAnimation(`${rotate180} 0.3s forwards`);
+    }
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div
+      css={css({
+        display: "flex",
+        flexDirection: "column",
+        gap: "0.2rem",
+      })}
+    >
+      <div
+        onClick={handleClickTitle}
+        css={css({
+          display: "flex",
+          gap: "1rem",
+          padding: "0.2rem 0rem",
+          alignItems: "center",
+          justifyContent: "space-between",
+          fontFamily: "'Noto Sans KR', sans-serif",
+          fontSize: "1.2rem",
+          // borderBottom: "0.1rem solid #F0F0F4",
+          cursor: "pointer",
+        })}
+      >
+        {title}
+        <svg
+          width={16}
+          height={16}
+          viewBox="0 0 20 20"
+          css={css({
+            animation: rotateAnimation,
+          })}
+        >
+          <path
+            d="M4 8L10 12L16 8"
+            fill={isOpen ? "#ffffff" : "#ffffff"}
+            stroke={isOpen ? "#000000" : "#000000"}
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            css={css({
+              transition: "0.1s",
+            })}
+          ></path>
+        </svg>
+      </div>
+      <div
+        css={css({
+          display: isOpen ? "" : "none",
+          fontFamily: "'Noto Sans KR', sans-serif",
+          fontSize: "1rem",
+        })}
+      >
+        <Box>{children}</Box>
+      </div>
+    </div>
+  );
+}
+
+export { Collapse, CollapseItem };
---
+++ src/stories/Collapse.stories.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { Input } from "../components/Input";
+import { Modal } from "../components/Modal";
+import { Button } from "../components/Button";
+import { Collapse, CollapseItem } from "../components/Collapse";
+
+function Demo() {
+  return (
+    <div>
+      <Collapse>
+        <CollapseItem title={"Q. 이건 무슨 기능이져"}>
+          컬렙슨데요. 발음이 이게 맞는진 ㅁ르겠는데 무튼 디자이너들이 부르는
+          이름입니다. 참고로 이거 처음 개발해봐요. 다른 컴포넌트는 잘만
+          만들었는데.ㅋㅋ
+        </CollapseItem>
+        <CollapseItem title={"Q. 이거 만든 사람이 누구인지 알 수 있을까요?"}>
+          이 디자인 시스템을 만든 사람의 정체는 한국에 거주중인 허형준입니다.
+          2020년에 1인 회사를 굉장히 실용적인 이유로 창업하고 현재까지 매출 없이
+          계속 도전해보고 있는 사람이죠. 중간에 돈 받고 프리랜서 해봤다가 이건
+          도저히 사람이 할게 아니라면서 떄려치고 이런거 만들고 있는 사람입니다.
+          근데 지금 취업할지 학교 계속 다닐지, 군대 갈지, 창업할지, 해외
+          유학갈지, 학회 논문 쓸지, 굉장히 많은 선택지 땜에 고심하고 있다고
+          합니다.
+        </CollapseItem>
+      </Collapse>
+    </div>
+  );
+}
+
+const meta = {
+  title: "Components/Collapse",
+  component: Demo,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof Collapse>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
