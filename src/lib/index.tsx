6987374eae77170939a2af3d6e5eca34552c6f41
Dipokal HHJ <hhj@devent.kr>
2024-04-09 16:18:15 +0900

    build: update version to 0.1.6

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,5 +1,6 @@
 import "./index.css";
 
+export * from "./components/A";
 export * from "./components/Badge";
 export * from "./components/Box";
 export * from "./components/Button";
@@ -7,18 +8,19 @@ export * from "./components/Checkbox";
 export * from "./components/Collapse";
 export * from "./components/Container";
 export * from "./components/Dialog";
+export * from "./components/Divider";
 export * from "./components/Dropdown";
 export * from "./components/Flex";
 export * from "./components/Input";
 export * from "./components/Modal";
 export * from "./components/Navbar";
+export * from "./components/Popover";
+export * from "./components/Progressbar";
 export * from "./components/Radio";
 export * from "./components/Skeleton";
 export * from "./components/Switch";
 export * from "./components/Table";
 export * from "./components/Toast";
 export * from "./components/Toggle";
-export * from "./components/Progressbar";
-export * from "./components/Divider";
 
 export * from "./hooks/useToast";
